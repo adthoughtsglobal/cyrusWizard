cba1a6c481451808ff8d1d30d344a8ffa278a2af
adthoughtsglobal <adthoughtsglobal@gmail.com>
2026-02-21 16:53:25 +0530

    better

--- cyrusAssets/script.js
+++ cyrusAssets/script.js
@@ -14,8 +14,8 @@ const loadAssets = async () => {
         document.body.appendChild(script);
     });
 
-    await loadCSS('cyrusAssets/styles.css');
-    await loadJS('cyrusAssets/reqs.js');
+    await loadCSS('cyrusWizard/cyrusAssets/styles.css');
+    await loadJS('cyrusWizard/cyrusAssets/reqs.js');
 };
 
 loadAssets().then(() => {
